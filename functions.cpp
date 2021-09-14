b551fca5bfb4839da272bf76d16c7f1f0c457a73
McNulty72 <42808045+McNulty72@users.noreply.github.com>
2021-09-14 19:34:25 +0200

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <iostream>
+
+void bleep(std::string word, std::string &text) {
+  std::string temp = "";
+
+  for(int i = 0; i <= text.size(); i++) {
+
+    if (text[i] == ' ' || text[i] == '.' || i == text.size()) {
+
+      if (temp == word) {
+
+        for (int j = 0; j < word.size(); j++) {
+
+          text[i-temp.size()+j] = '*';
+
+        }
+
+      }
+
+       temp = "";
+
+    }
+    else {
+
+        bool flag = false;
+        int k = 0;
+
+        while(!flag && k < word.size()){
+
+            if (text[i] == word[k]){
+
+                temp += text[i];
+                flag = true;
+
+            }
+
+            k++;
+
+        }
+                
+    }
+    std::cout << temp << "\n";
+  }
+
+}
